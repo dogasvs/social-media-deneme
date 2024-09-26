cfd02dc56893b2b8427549fbcbb89695ab1a3256
Doğa Savaş <doasavaas@gmail.com>
2024-09-26 16:34:21 +0300

    users cardlara css eklendi

---
+++ app/page.jsx
@@ -0,0 +1,31 @@
+import Location from "@/svgs/location/page"
+import School from "@/svgs/school/page";
+import Link from "next/link";
+
+export default async function Home() {
+  const { users } = await fetch("https://dummyjson.com/users").then(res => res.json());
+
+  return (
+    <div className="homeContainer">
+      {users.map(x => (
+        <div className="users" key={x.id}>
+          <div className="usersImportInfo">
+          <img src={x.gender === "female" ? "/female.png" : "/man.png"}
+            alt={`${x.firstName}'s profile image`}
+            className="profileImage"
+          />
+            <h3>{x.firstName} {x.lastName}</h3>
+            <strong>@{x.username}</strong>
+
+          </div>
+          <div className="userInform">
+          
+            <div className="location"> <Location /> <p> {x.address.country}</p> </div>
+            <div className="location"> <School /> <p> {x.university}</p> </div>
+          </div>
+            <Link href="#" className="seeProfile">see profile</Link>
+        </div>
+      ))}
+    </div>
+  );
+}
